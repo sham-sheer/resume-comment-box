30e18eeb178b085b41fcbc386026c49f7ec62dd4
Julio Cesar Fausto <jcfausto@gmail.com>
2015-09-12 14:14:58 -0300

    Add CommentList to CommentBox

--- public/js/components.js
+++ public/js/components.js
@@ -13,7 +13,8 @@ var CommentBox = React.createClass({
   render: function(){
     return(
       <div className="commentBox">
-        Hello, world! I am an react-js comment box component!
+        <h1>Comments</h1>
+        <CommentList />
       </div>
     );
   }
