3d5be6ef65e08c2013f48836ba722a01bbc36528
Julio Cesar Fausto <jcfausto@gmail.com>
2015-09-12 16:07:54 -0300

    Fix some typos and missing attributes and also fix some bugs in the server. Now everything is working

--- public/js/components.js
+++ public/js/components.js
@@ -25,16 +25,16 @@ var CommentForm = React.createClass({
     if (!text || !author) {
       return;
     }
-    this.props.onCommentSubmit({autor: author, text: text});
+    this.props.onCommentSubmit({author: author, text: text});
     React.findDOMNode(this.refs.author).value = '';
     React.findDOMNode(this.refs.text).value = '';
     return;
   },
   render: function() {
     return(
-      <form className="commentForm">
-        <input type="text" placeholder="Your name please..." />
-        <input type="text" placeholder="Your comment..."/>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name please..." ref="author" />
+        <input type="text" placeholder="Your comment..." ref="text" />
         <input type="submit" value="Post"/>
       </form>
     );
--- server.rb
+++ server.rb
@@ -15,12 +15,15 @@ puts "Super Simple Ruby Http Server started: http://localhost:#{port}/"
 # root directory set do ./puglic by default.
 root = File.expand_path './public'
 
+# comments resource path
+comments_path = "#{root}/comments.json";
+
 # creating the server is a piece of cake!
 server = WEBrick::HTTPServer.new Port: port, DocumentRoot: root
 
 # Add the ability to respond to a POST request on comments.json
 server.mount_proc '/comments.json' do |req, res|
-  comments = JSON.parse(File.read("#{root}/comments.json"))
+  comments = JSON.parse(File.read(comments_path));
 
   if req.request_method == 'POST'
     # Assume it's well formed
@@ -29,7 +32,7 @@ server.mount_proc '/comments.json' do |req, res|
       comment[key] = value.force_encoding('UTF-8')
     end
     comments << comment
-    File.write('./comments.json', JSON.pretty_generate(comments, indent: '    '))
+    File.write(comments_path, JSON.pretty_generate(comments, indent: '    '))
   end
 
   # always return json
