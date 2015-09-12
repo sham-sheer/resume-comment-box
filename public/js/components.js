c77c9f02a427de297c26de7d3788fc1c61957bff
Julio Cesar Fausto <jcfausto@gmail.com>
2015-09-12 15:20:49 -0300

    Extracted the Ajax request to a method and added a polling to the server at regular intervals in order to get live updates. Change your comments.json file and see what happens. In real app, maybe WebSockets could be used instead of a polling strategy.

--- public/js/components.js
+++ public/js/components.js
@@ -44,10 +44,7 @@ var Comment = React.createClass({
 });
 
 var CommentBox = React.createClass({
-  getInitialState: function() {
-    return {data: []}
-  },
-  componentDidMount: function(){
+  loadCommentsFromServer: function() {
     $.ajax({
       url: this.props.url,
       dataType: 'json',
@@ -59,6 +56,13 @@ var CommentBox = React.createClass({
         console.error(this.props.url, status, err.toString());
       }.bind(this)
     });
+  },
+  getInitialState: function() {
+    return {data: []}
+  },
+  componentDidMount: function(){
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   },
   render: function(){
     return(
@@ -72,6 +76,6 @@ var CommentBox = React.createClass({
 
 });
 React.render(
-  <CommentBox url="comments.json" />,
+  <CommentBox url="comments.json" pollInterval={2000} />,
   document.getElementById('content')
 );
